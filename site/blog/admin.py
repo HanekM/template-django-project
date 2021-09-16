32a4c1fc35793e015de9cea2d9724f4c4aa23ccf
HanekM <bakklazhan3@gmail.com>
2021-09-16 20:56:57 +0300

    Add models, admins, forms, inlines

--- site/blog/admin.py
+++ site/blog/admin.py
@@ -1,1 +1,61 @@
 from django.contrib import admin
+from django.shortcuts import get_object_or_404
+from django.utils.translation import ugettext_lazy as _
+
+from blog.models import (
+    Article,
+    Category,
+    ArticleImage
+)
+from blog.forms import ArticleImageForm
+
+
+class ArticleImageInline(admin.TabularInline):
+    model = ArticleImage
+    form = ArticleImageForm
+    extra = 0
+    fieldsets = (
+        ('Image', {
+                'fields': ('title', 'image',),
+            }
+        ),
+    )
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'date_published', 'category', 'slug', 'main_page')
+    inlines = [ArticleImageInline]
+    multiupload_form = True
+    multiupload_list = False
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('category',)
+    fieldsets = (
+        ('', {
+            'fields': ('title', 'description', 'category', 'main_page'),
+        }),
+            ((_('Additionally')), {
+            'classes': ('grp-collapse grp-closed',),
+            'fields': ('slug',),
+        }),
+    )
+
+    def delete_file(self, pk, request):
+        '''
+        Delete an image
+        '''
+        obj = get_object_or_404(ArticleImage, pk=pk)
+
+        return obj.delete()
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category')
+    fieldsets = (
+        ('', {
+            'fields': ('category', ),
+        }),
+    )
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
--- site/blog/models.py
+++ site/blog/models.py
@@ -1,2 +1,81 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 
+
+class Category(models.Model):
+    category = models.CharField(
+        verbose_name=_('Categories'), max_length=250, help_text=_('No more than 250 characters')
+    )
+
+    objects = models.Manager()
+
+    class Meta:
+        verbose_name = _('News catagory')
+        verbose_name_plural = _('News catagories')
+
+    def __str__(self):
+        return self.category
+
+
+class Article(models.Model):
+    title = models.CharField(
+        _('Title'), max_length=250, help_text=_('No more than 250 characters')
+    )
+    description = models.TextField(verbose_name=_('Description'), blank=True,)
+    date_published = models.DateTimeField(_('Date published'), auto_now=True)
+    slug = models.SlugField(_('Slag'), unique_for_date='date_published')
+    main_page = models.BooleanField(_('Main'), default=False, help_text=_('Show'))
+    category = models.ForeignKey(
+        Category, 
+        related_name='news', 
+        blank=True, null=True,
+        verbose_name=_('Categories'),
+        on_delete=models.CASCADE
+    )
+
+    objects = models.Manager()
+
+    class Meta:
+        ordering = ('-date_published', )
+        verbose_name = _('Article')
+        verbose_name_plural = _('Articles')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        try:
+            url = reverse('news-detail', kwargs={
+                    'year': self.pub_date.strftime('%Y'),
+                    'month': self.pub_date.strftime('%m'),
+                    'day': self.pub_date.strftime('%d'),
+                    'slug': self.slug,
+                }
+            )
+        except Exception:
+            url = '/'
+        return url
+
+
+class ArticleImage(models.Model):
+    article = models.ForeignKey(Article,
+        verbose_name=_('Article'),
+        related_name='images',
+        on_delete=models.CASCADE
+    )
+    image = models.ImageField(_('Image'), upload_to='images')
+    title = models.CharField(
+        _('Title'), blank=True, max_length=250, help_text=_('No more than 250 characters')
+    )
+
+    class Meta:
+        verbose_name = _('Article image')
+        verbose_name_plural = _('Article images')
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def filename(self):
+        return self.image.name.rsplit('/', 1)[-1]
